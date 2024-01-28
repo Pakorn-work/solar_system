f033342487582c8a97c54d3031cc200d93aad1fe
NuTtFromNoWhere <warasinchai@gmail.com>
2024-01-28 20:08:13 +0700

    nig

--- main.js
+++ main.js
@@ -226,13 +226,13 @@ document.addEventListener("mousemove", onPointerMove);
 
 
 // Function to handle window resize
-  function handleResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-  }
+function handleResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
 
-  window.addEventListener('resize', handleResize);
+window.addEventListener('resize', handleResize);
 
 
 
